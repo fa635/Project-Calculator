4b822777414b9c88c3b72b6d64d5082ddd7fa18b
fa635 <bfalonne823@gmail.com>
2024-01-07 10:35:13 +0100

    Add eventListeners for buttons

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ let operator = "+" || "-" || "*" ||  "/";
 
 
 function operate(a, operator, b) {
-    a = +prompt("a");
-    operator = prompt("operator")
-    b = +prompt("b");
+    // a = +prompt("a");
+    // operator = prompt("operator")
+    // b = +prompt("b");
     if (operator === "+") {
         console.log(add(a, b))
 
@@ -54,8 +54,64 @@ function operate(a, operator, b) {
 }
 operate(a, operator, b)
 
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const two = document.querySelector(".two");
+// two.addEventListener("click", displayValue);
+
+// const three = document.querySelector(".three");
+// three.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", displayValue);
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button =>{
+    button.addEventListener("click", function(){
+        displayValue(button.innerHTML)
+    });
+});
 
 
+function displayValue(btn) {
+    // const buttons = document.querySelectorAll(".one");
+
+    const display = document.querySelector(".display");
+    display.textContent = btn;
+
+}
 
   
   
