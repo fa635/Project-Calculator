b3f5bada614c1de7d64f37860357b6f72645e544
fa635 <bfalonne823@gmail.com>
2024-01-07 06:39:28 +0100

    Create operate function

--- script.js
+++ script.js
@@ -21,6 +21,41 @@ const divide = function(a, b) {
 
 
 
+let a = 0;
+let b = 0;
+let operator = "+" || "-" || "*" ||  "/";
+
+
+
+function operate(a, operator, b) {
+    a = +prompt("a");
+    operator = prompt("operator")
+    b = +prompt("b");
+    if (operator === "+") {
+        console.log(add(a, b))
+
+    }
+
+    if (operator === "-") {
+        console.log(subtract(a, b))
+
+    }
+
+    if (operator === "*") {
+        console.log(multiply(a, b))
+
+    }
+
+    if (operator === "/") {
+        console.log(divide(a, b))
+
+    }
+    
+}
+operate(a, operator, b)
+
+
+
 
   
   
