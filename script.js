581afb6c01d6650edf4faaddd3333ff90e1d82df
fa635 <bfalonne823@gmail.com>
2024-01-10 11:20:20 +0100

    Delete unnecessary commits and code

--- script.js
+++ script.js
@@ -33,16 +33,14 @@ let array;
 
 let total = 0;
 
-//let clicked = false;
+
 
 
 
 
 
 function operate() {
-    // a = +prompt("a");
-    // operator = prompt("operator")
-    // b = +prompt("b");
+    
 
     const display = document.querySelector(".display");
 
@@ -81,7 +79,7 @@ function operate() {
     }
     
 }
-operate(a, operator, b)
+
 
 
 
@@ -101,7 +99,7 @@ buttons.forEach(button =>{
 
 
 function displayValue(btn) {
-    // const buttons = document.querySelectorAll(".one");
+
 
     const display = document.querySelector(".display");
     
@@ -133,57 +131,14 @@ function displayValue(btn) {
                 e = total + btn;
                 display.textContent = e;
             }
-            // else if (array[1] !== "" && b === +array[1]) {
-            //     operate();
-            //     e = total + btn;
-            //     display.textContent = e;
-            // }
-
-
-
-
-            // if  eaary 1 is not "" and b === array 1
             
-            //a !== 0 && b !== 0
 
-            //if the avant dernier of e is equal to b
-            //+e.slice(-2, -1) === array[1]
-
-            //e.split(/(+|-|/|*)/ a === e.split(/(+|-|/|*)/[0] && b === e.split(/(+|-|/|*)/[1])
-            // 
-
-            
-            
+  
         }
-        // else if (clicked === true) {
-        //     e = e;
-        //     display.textContent = e;
-        // }
-
+        
     }
 
-    //fix the equal thing where the total part gets calculated into the e. Think what s the difference between them a their respective stages and place a if statement
     
-    // if (a === e.split(/[+-\/*]+/)[0]) {
-    //     operate();
-    //     e = total + btn;t 
-    //     display.textContent = e;
-    // }
-
-
-    // && b === e.split(/[+-\/*]+/)[1].slice(-1)
-
-    // if (e !== " ") {
-    //     e = e + btn;
-    //     display.textContent = e;
-    // }
-
-    // if (total !== 0) {
-    //     if (a !== 0 && b !== 0) {
-    //     operate();
-    //     e = total + e.slice(-1);
-    //     }
-    // }
 
     
     if (e.includes("+")) {
@@ -214,80 +169,28 @@ function displayValue(btn) {
 
 
     if (e.slice(-1) === "+" || e.slice(-1) === "-" || e.slice(-1) === "*" || e.slice(-1) === "/") {
+
         if (array[1].includes("+") || array[1].includes("-") || array[1].includes("*") || array[1].includes("/")) {
-        b = +array[1].slice(0, -1);
 
-        operate();
+            b = +array[1].slice(0, -1);
 
-        e = total + btn;
-        display.textContent = e;
+            operate();
 
-      
+            e = total + btn;
+            display.textContent = e;
     
         }
     }
 
-
-
     
-
-
-
-    // i need a way to make it execute the operation as soon as it has two values so that solution can be reassigned to a or array[0]
-
-    // if (array !== undefined) {
-    //     a = +array[0];
-    //     b = +array[1];
-    // }
-
-    // if (e.includes("+") && e.includes("-")) {
-    //     a = a + b
-    // }
-
-    
-
-    // a = array[0];
-    // b = array[1];
-
-
-
-    
-
-    // if (a === " ") {
-    //     a = btn;
-    // }
-    // else if (btn !== "+" && btn !== "-" && btn !== "*" && btn !== "/" && operator === " ") {
-    //     a = a + btn;
-
-    // }
-    // else if (btn === "+" || btn === "-" || btn === "*" || btn === "/") {
-    //     operator = btn;
-    // }
-    // else if (a !== " " && operator !== " " && b === " ") {
-    //     b = btn;
-    // }
-    // else if (b !== " ") {
-    //     b = b + btn;
-    // }
-
-
-
-    
-
-    
-
-    //if (btn === "+" || btn === "-" || btn === "*" || btn === "/")
 }
 
-//assign btn to a,b and operator ?
+
 
   
 
 const equal = document.querySelector(".equal");
-equal.addEventListener("click", function() {
-    operate();
-    // clicked = true;
-});
+equal.addEventListener("click", operate);
 
 
 const clear = document.querySelector(".clear");
